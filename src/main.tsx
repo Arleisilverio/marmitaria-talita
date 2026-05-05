a2afc5e1e1722c921f7a4a4a9107178634dc744b
[dyad] <arleisilverio41@gmail.com>
2026-05-04 21:27:36 -0300

    [dyad] Corrigindo arquivo main.tsx que não possuía a rota de login nem o sistema de sessão do App.tsx - wrote 1 file(s)

--- src/main.tsx
+++ src/main.tsx
@@ -1,24 +1,10 @@
 import React, { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { Toaster } from 'react-hot-toast';
-import { CartProvider } from './contexts/CartContext';
-import ClientHome from './pages/ClientHome';
-import ClientCheckout from './pages/ClientCheckout';
-import AdminDashboard from './pages/AdminDashboard';
+import App from './App';
 import './index.css';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <CartProvider>
-      <Toaster position="top-center" reverseOrder={false} />
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<ClientHome />} />
-          <Route path="/checkout" element={<ClientCheckout />} />
-          <Route path="/admin" element={<AdminDashboard />} />
-        </Routes>
-      </BrowserRouter>
-    </CartProvider>
+    <App />
   </StrictMode>,
 );
